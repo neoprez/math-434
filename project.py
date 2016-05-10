3e4368a732d9d5e90ccf47d20148c53ed42cec9c
Rodny Perez <rodnyeperez@gmail.com>
2016-05-10 17:11:49 -0400

    Add ugly code get montly average accross years

--- project.py
+++ project.py
@@ -1,4 +1,5 @@
 import csv
+import numpy as np
 import matplotlib.pyplot as plt
 
 states = ["california", "michigan", "new_york", 
@@ -10,8 +11,9 @@ states_upper = { "california": "CALIFORNIA", "michigan":"MICHIGAN", "new_york":"
 								"washington":"WASHINGTON"} 
 
 path = "data/"
-# ignore year 2000
 #d[state][year][period]=val
+# dict[state][year][month][mean]
+state_temperatures = {}
 apples_info = {}
 periods = {"JAN":1, "FEB":2, "MAR":3, "APR":4, "MAY":5, "JUN":6, "JUL":7, "AUG":8, "SEP":9, "OCT":10, "NOV":11, "DEC":12}
 
@@ -34,8 +36,6 @@ with open(path+"apple-2000-2010.csv") as f:
 		apples_info[state][row['Year']][periods[row['Period']]-1] = val
 
 #print apples_data
-# dict[state][year][month][mean]
-state_temperatures = {}
 for state in states:
 	state_temperatures[state] = {}
 	with open(path + "anual-" + state + "-2000-2010.csv") as f:
@@ -68,7 +68,26 @@ for state in states:
 
 			state_temperatures[state][year][m-1] = mean
 
-def print_state_temp_as_table(data):
+def get_average(data):
+	return sum(data)/float(len(data))
+
+def get_averages_per_month_across_years(data):
+	state_averages = {}
+	for s in data.keys():
+		years = sorted(data[s].keys())
+		data_for_years = []
+		state_averages[s] = []
+		for year in years:
+			data_for_years.append(data[s][year])
+
+		data_for_years = np.array(data_for_years)
+		for i in range(12):
+			state_averages[s].append(get_average(data_for_years[:,i]))
+		
+	return state_averages	
+
+
+def print_state_temp_as_table(data, averages_per_month):
 	ms = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]
 	for s in data.keys():
 		print "\t"*6,states_upper[s]
@@ -76,12 +95,17 @@ def print_state_temp_as_table(data):
 		print "    \t", "\t".join(ms)
 		print "_" * 100
 		years = sorted(data[s].keys())
+		data_for_years = []
 		for year in years:
-			print year+"\t", "\t".join(map(str,data[s][year]))
+			data_year = data[s][year]
+			print year+"\t", "\t".join(map(str,data_year))
 		print "-" * 100	
+		print "AVG:\t", "\t".join(map(str,averages_per_month[s]))
+		print "*" * 100
 
-
+monthly_state_temp_averages = get_averages_per_month_across_years(state_temperatures)
+monthly_state_apples_averages = get_averages_per_month_across_years(apples_info)
 print "^" * 43, "TEMPERATURES", "^" * 43
-print_state_temp_as_table(state_temperatures)
+print_state_temp_as_table(state_temperatures, monthly_state_temp_averages)
 print "#" * 43, "APPLE PRICES", "#" * 43
-print_state_temp_as_table(apples_info)
+print_state_temp_as_table(apples_info, monthly_state_apples_averages)
